a11c7fddd214949bda36c6d71100a913edd4b5f8
Guest 01 <guest01@alunos.sc.senac.br>
2022-09-14 21:38:37 -0300

    primeira aula

---
+++ src/main/java/com/entra21OlaMundo/entra21OlaMundo/Entra21OlaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.entra21OlaMundo.entra21OlaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entra21OlaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entra21OlaMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entra21OlaMundo/entra21OlaMundo/Entra21OlaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21OlaMundo.entra21OlaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entra21OlaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
